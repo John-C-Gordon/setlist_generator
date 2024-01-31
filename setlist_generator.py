5eb74f59c7798b6ce0e1ffe4afbbe2a93ef2f49e
John Gordon <jcg4sg@virginia.edu>
2024-01-31 15:40:23 -0500

    Update setlist_generator.py

--- setlist_generator.py
+++ setlist_generator.py
@@ -7,7 +7,7 @@ import time as t
 from datetime import datetime
 
 df = pd.read_csv('cougar_songs.csv', index_col=False)
-st.set_page_config(page_icon=':bar_chart:')
+st.set_page_config(page_icon='🤩')
 
 with st.container():
 
