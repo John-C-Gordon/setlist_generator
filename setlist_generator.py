c3a8c343ce1cde26a39720213427c3c7be552589
John Gordon <jcg4sg@virginia.edu>
2024-02-17 14:24:59 -0500

    Update setlist_generator.py

--- setlist_generator.py
+++ setlist_generator.py
@@ -55,7 +55,7 @@ with st.container():
         m, s = divmod(sum, 60)
 
         st.markdown('''
-            **Total:** :red[{}] minutes :red[{}] seconds'''.format(m, s))
+            **Total:** :red[{}] minutes'''.format(round(sum/60, 2))
         # st.dataframe(covers.groupby(['Artist Name(s)'].count()['Count'])
                  
         
